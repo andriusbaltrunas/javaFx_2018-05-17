f9482b5c3d39e96c8e89a97f322ccb5476ea381a
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2018-05-17 20:17:45 +0300

    init commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+public class Controller {
+
+    @FXML
+    private Label dummyLabel;
+
+    @FXML
+    private TextField nameTextField;
+
+    public void sayHi(ActionEvent event) {
+        dummyLabel.setText("Labas Andriau");
+    }
+
+    public void changeName(ActionEvent event) {
+        String name = nameTextField.getText();
+        dummyLabel.setText("LABUKAS " + name);
+    }
+
+    public void changeNameOnEnter(KeyEvent event){
+        if(event.getCode().equals(KeyCode.ENTER)){
+            dummyLabel.setText("Laba "+nameTextField.getText());
+        }
+    }
+}
